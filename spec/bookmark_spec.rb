a89d621468ac4fd9025d19bcfc8e6417c1a63337
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-06-04 16:55:11 +0100

    setup testing environment and debugging

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,12 +2,21 @@ require 'bookmark'
 
 describe Bookmark do
   describe '.all' do
-    it 'returns all bookmarks' do
-      bookmarks = Bookmark.all
+    it 'returns all bookmarks in an array' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
 
-      expect(bookmarks).to include("http://makersacademy.com")
-      expect(bookmarks).to include("http://destroyallsoftware.com")
-      expect(bookmarks).to include("http://google.com")
+      # Add the test data
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES('http://google.com');")
+
+      expected_bookmarks = [
+        'http://makersacademy.com',
+        'http://destroyallsoftware.com',
+        'http://google.com'
+      ]
+
+      expect(Bookmark.all).to eq expected_bookmarks
     end
   end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -2,9 +2,19 @@
 # So that I can quickly go to web sites I regularly visit
 # I would like to see a list of bookmarks on the homepage
 
-feature 'View bookmark' do
+require 'pg'
+
+feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://google.com');")
+
     visit('/bookmarks')
+
     expect(page).to have_content "http://makersacademy.com"
     expect(page).to have_content "http://destroyallsoftware.com"
     expect(page).to have_content "http://google.com"
